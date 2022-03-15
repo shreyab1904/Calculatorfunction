c5d3bc51e6ab27bf24d568b53f83388bd80cb4a2
shreyab1904 <shreyaborkar0201@gmail.com>
2022-03-15 17:10:31 +0530

    Inheritance added

--- src/com/harman/batch4/Calculator.java
+++ src/com/harman/batch4/Calculator.java
@@ -2,26 +2,21 @@ package com.harman.batch4;
 
 import java.util.Scanner;
 
-public class Calculator{
+public class Calculator extends ParentCalculator{
     public static void main(String[] args) {
 
-        Addition adCalc =new Addition();
-        Subtraction subCalc=new Subtraction();
-        Multiplication multCalc=new Multiplication();
-        Division divCalc=new Division();
+       Calculator ob =new Calculator();
         Scanner input=new Scanner((System.in));
-        int n1,n2,result1,result2,result3,result4;
+        int n1,n2,result1,result2;
         System.out.println("Enter number : ");
         n1=input.nextInt();
         System.out.println("Enter number 2:");
         n2=input.nextInt();
-        result1=adCalc.AdditionOperation(n1,n2);
-        result2=subCalc.SubOperation(n1,n2);
-        result3=multCalc.multiOperation(n1,n2);
-        result4=divCalc.divOperation(n1,n2);
+        result1=ob.AdditionOperation(n1,n2);
+        result2=ob.SubOperation(n1,n2);
+
         System.out.println(result1);
         System.out.println(result2);
-        System.out.println(result3);
-        System.out.println(result4);
+
     }
 }
---
+++ src/com/harman/batch4/ParentCalculator.java
@@ -0,0 +1,17 @@
+package com.harman.batch4;
+
+public class ParentCalculator {
+
+        public int AdditionOperation(int x,int y){
+            return x+y;
+        }
+
+        public int SubOperation(int x,int y){
+            return x-y;
+        }
+        public int multiOperation(int x,int y){
+            return x*y;
+        }
+    }
+
+
