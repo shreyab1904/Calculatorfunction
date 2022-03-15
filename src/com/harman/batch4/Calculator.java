e465c172963c88c16525a103619a3d76b407c66e
shreyab1904 <shreyaborkar0201@gmail.com>
2022-03-15 15:52:52 +0530

    Addition operation code completed

--- src/com/harman/batch4/Calculator.java
+++ src/com/harman/batch4/Calculator.java
@@ -2,16 +2,23 @@ package com.harman.batch4;
 
 import java.util.Scanner;
 
-public class Calculator {
+public class Calculator{
+    public int AdditionOperation(int x,int y)
+    {
+        int z=x+y;
+        return z;
+    }
+
     public static void main(String[] args) {
 
+        Calculator calc=new Calculator();
         Scanner input=new Scanner((System.in));
         int n1,n2,result;
         System.out.println("Enter number : ");
         n1=input.nextInt();
         System.out.println("Enter number 2:");
         n2=input.nextInt();
-        result=n1+n2;
+        result=calc.AdditionOperation(n1,n2);
         System.out.println(result);
     }
 }
